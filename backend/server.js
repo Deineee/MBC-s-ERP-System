a6357e4867c99ac88dafceeea53fa82a702d77b1
Deineee <deinepomperada33@gmail.com>
2025-01-07 21:29:33 +0800

    update server.js

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,16 @@
+require('dotenv').config()
+
 const express = require('express')
 
 //express app
 const app = express()
 
+//test
+app.get('/', (req, res) => {
+    res.json({mssg: 'whats up'})
+})
+
+app.listen(process.env.PORT, () => {
+    console.log('heyy', process.env.PORT)
+})
+process.env
