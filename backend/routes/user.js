2c9dc4b7e7cbd0c042e1f8ff5d8951dc45a08909
Deineee <deinepomperada33@gmail.com>
2025-01-09 04:23:58 +0800

    user controller and router done

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -1,31 +1,28 @@
 const express = require('express')
+
 const {
     createUser,
+    getUsers,
+    getUser,
+    deleteUser,
+    updateUser
 } = require('../controllers/userController')
 
 const router = express.Router()
 
-//sample for getting all user manage
-router.get('/', (req, res) => {
-    res.json({mssg: 'GET all user manage'})
-})
+//sample for getting all user 
+router.get('/', getUsers)
 
-//sample for getting single user manage
-router.get('/:id', (req, res) =>{
-    res.json({mssg: 'GET single user manage'})
-})
+//sample for getting single user 
+router.get('/:id', getUser)
 
-//sample POST a  new user manage
+//sample POST a  new user 
 router.post('/', createUser)
 
-//sample DELETE a  new user manage
-router.delete('/:id', (req, res) => {
-    res.json({mssg: 'DELETE a new user manage'})
-})
+//sample DELETE a  new user
+router.delete('/:id', deleteUser)
 
-//sample UPDATE a  new user manage
-router.patch('/', (req, res) => {
-    res.json({mssg: 'UPDATE a new user manage'})
-})  
+//sample UPDATE a  new user 
+router.patch('/:id', updateUser) 
 
 module.exports = router
