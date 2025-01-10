e07294497d78dfaae3b673cc15a612afa21dc19b
Deineee <deinepomperada33@gmail.com>
2025-01-10 15:09:58 +0800

    created component directory with Navbar

---
+++ frontend/src/components/Navbar.js
@@ -0,0 +1,16 @@
+import { Link } from 'react-router-dom'
+
+const Navbar = () => {
+
+    return (
+        <header>
+            <div className="container">
+                <Link to="/">
+                    <h1>
+                        MBC System {/* Please change this frontend developerss ;) */}
+                    </h1>
+                </Link>
+            </div> 
+        </header>
+    )
+}
