181c22265130eef210cbfa548637311b452732ad
Deineee <deinepomperada33@gmail.com>
2025-01-13 01:43:31 +0800

    created navbar and siebar components. added functionality to logout

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,16 +1,37 @@
-import {BrowserRouter, Routes, Route} from 'react-router-dom'
-
-import Login from './pages/Login'
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'
+import { useAuthContext } from './hooks/useAuthContext';  
+// pages & components
+import Login from './pages/Login';
 import SignUp from './pages/Signup';
+import Dashboard from './pages/Dashboard'; 
 
 function App() {
+  const { user } = useAuthContext(); 
+
   return (
     <div className="App">
       <BrowserRouter>
         <div className="pages">
           <Routes>
-            <Route path="/login" element={<Login />} />
+            {/* Public Routes */}
+            <Route path="/" element={<Login />} />
             <Route path="/signup" element={<SignUp />} />
+
+            {/* Protected Route for Dashboard */}
+            <Route
+              path="/dashboard"
+              element={
+                user ? (
+                  <div className="app-container">
+                    <div className="main-content">
+                      <Dashboard /> {/* Dashboard content */}
+                    </div>
+                  </div>
+                ) : (
+                  <Navigate to="/" /> // Redirect to login if not authenticated
+                )
+              }
+            />
           </Routes>
         </div>
       </BrowserRouter>
