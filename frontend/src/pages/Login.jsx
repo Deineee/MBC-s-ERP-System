598c432a484c1d79be1a79c6208b0d9b16905734
shaine-png <shaine.png@gmail.com>
2025-01-17 00:07:21 +0800

    login page design update

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -4,23 +4,31 @@ import { useLogin } from "../hooks/useLogin";
 
 import '../css/Login.css';
 
+import mbcLogo from'../images/mbc-horizontal-logo.png';
+import greyBackground from '../images/grey-background.svg';
+
 
 const Login = () => {
 
-  return (
-  <div>
-    <div className="leftpanel">
-      <h1>MBB System</h1>
-    </div>
-
+  return ( 
     
+    <div className="login-page">
+      <div className="login-container">
 
-    <Panel/>
-  </div>
+      <img src={mbcLogo} alt="MBC Logo" className="mbc-logo"/>
+
+      </div>
+
+      <div className="login-panel">
+        <Panel />
+      </div>
+
+    </div>
+    
   );
 };
 
-const Panel = () => {
+ const Panel = () => {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
   const { login, error, isLoading } = useLogin();
@@ -36,25 +44,26 @@ const Panel = () => {
     }
   };
   return (  <form className="login" onSubmit={handleSubmit}>
-    <h3>Log In</h3>
+    <h3>Welcome Back Ka-Metrix!</h3>
     
-    <label>Email address:</label>
+    <label>Email: <span className="highlight-text">(issued by the company)</span></label>
     <input 
       type="email" 
       onChange={(e) => setEmail(e.target.value)} 
       value={email} 
     />
     <label>Password:</label>
-    <input 
+    <input className="password"
       type="password" 
       onChange={(e) => setPassword(e.target.value)} 
       value={password} 
     />
+<label className="forgot-password">Forgot password?</label>
 
     <button disabled={isLoading}>Log in</button>
     {error && <div className="error">{error}</div>}
   </form>
-  );
+  ); 
 };
 
 export default Login;
